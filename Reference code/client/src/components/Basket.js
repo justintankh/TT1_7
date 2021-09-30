7cfa65cb5ddad7a955cd9b7ad4cf45702c2116d5
Wang Piao <piao.wang.2018@sis.smu.edu.sg>
2021-09-30 15:55:42 +0800

    Update Basket.js
    Removed the taxPrice and shippingPrice from the Basket.js template as it was not in the requirements, change the item.name to item.title to display item name.

--- Reference code/client/src/components/Basket.js
+++ Reference code/client/src/components/Basket.js
@@ -3,9 +3,8 @@ import React from 'react';
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
-  const taxPrice = itemsPrice * 0.14;
-  const shippingPrice = itemsPrice > 2000 ? 0 : 20;
-  const totalPrice = itemsPrice + taxPrice + shippingPrice;
+  const totalPrice = itemsPrice;
+
   return (
     <aside className="block col-1">
       <h2>Cart Items</h2>
@@ -13,7 +12,7 @@ export default function Basket(props) {
         {cartItems.length === 0 && <div>Cart is empty</div>}
         {cartItems.map((item) => (
           <div key={item.id} className="row">
-            <div className="col-2">{item.name}</div>
+            <div className="col-2">{item.title}</div>
             <div className="col-2">
               <button onClick={() => onRemove(item)} className="remove">
                 -
@@ -24,7 +23,7 @@ export default function Basket(props) {
             </div>
 
             <div className="col-2 text-right">
-              {item.qty} x ${item.price.toFixed(2)}
+              {item.qty} x ${item.price}
             </div>
           </div>
         ))}
@@ -32,20 +31,6 @@ export default function Basket(props) {
         {cartItems.length !== 0 && (
           <>
             <hr></hr>
-            <div className="row">
-              <div className="col-2">Items Price</div>
-              <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Tax Price</div>
-              <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Shipping Price</div>
-              <div className="col-1 text-right">
-                ${shippingPrice.toFixed(2)}
-              </div>
-            </div>
 
             <div className="row">
               <div className="col-2">
