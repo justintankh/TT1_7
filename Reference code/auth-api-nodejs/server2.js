d0f5927ddb29deda2b7a34df7ec884ff5e8405cf
justintankh <66722854+justintankh@users.noreply.github.com>
2021-09-30 13:36:50 +0800

    integrated user database

--- Reference code/auth-api-nodejs/server2.js
+++ Reference code/auth-api-nodejs/server2.js
@@ -52,9 +52,9 @@ const userData = {
 // enable CORS
 app.use(cors());
 // parse application/json
-app.use(express.json());
+app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //middleware that checks if JWT token exists and verifies it if it does exist.
@@ -100,7 +100,7 @@ app.post('/users/signin', function (req, res) {
   }
 
   usersData.forEach(User => {
-    if(user == User.username || pwd == User.password){
+    if(user == User.username && pwd == User.password){
       // generate token
       const token = utils.generateToken(userData);
       // get basic user details
