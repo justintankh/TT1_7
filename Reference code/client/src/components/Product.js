d9b9223919098751465809de47d7997ea11be0aa
ZhengTian <38405540+tangzt96@users.noreply.github.com>
2021-09-30 16:29:35 +0800

    slight formating on frontend product -wang

--- Reference code/client/src/components/Product.js
+++ Reference code/client/src/components/Product.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Product(props) {
   const { product, onAdd } = props;
   return (
-    <div>
+    <div className="block col-3">
       <img className="small" src={product.image} alt={product.name} />
       <h3>{product.title}<hr></hr></h3>
       <div>Price: ${product.price}</div>
